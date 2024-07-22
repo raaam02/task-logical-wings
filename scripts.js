2fe154533ffc56b11b97f578519dde300b1c832a
raaam02 <kingk9869991@gmail.com>
2024-07-22 23:41:17 +0530

    ajax added

---
+++ scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  // fetch countries data
+  $.ajax({
+    type: "method",
+    url: "fetchCountries.php",
+    success: function (response) {
+      $("#country").html(response);
+    },
+  });
+
+  // fetch state data
+  $("#country").change(function (e) {
+    const countryId = $(this).val();
+    $.ajax({
+      type: "method",
+      url: `fetchState.php?countryId=${countryId}`,
+      success: function (response) {
+        $("#state").html(response);
+      },
+    });
+  });
+
+  //   fetch city data
+  $("#state").change(function (e) {
+    const stateId = $(this).val();
+    $.ajax({
+      type: "method",
+      url: `fetchCity.php?countryId=${stateId}`,
+      success: function (response) {
+        $("#state").html(response);
+      },
+    });
+  });
+});
